7ecd8e660aa1647c49637396342f055a56be2a3a
Sivantha Paranavithana <sivantha.p@eatmeglobal.com>
2024-11-02 12:26:07 +0530

    Initial commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sivantha Paranavithana on 2024-11-02.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(Circle()).overlay {
+            Circle().stroke(.white, lineWidth: 4)
+        }.shadow(radius: 7)
+        
+    }
+}
+
+#Preview {
+    CircleImage()
+}
