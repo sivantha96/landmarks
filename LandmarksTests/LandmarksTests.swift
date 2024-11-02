4f62ec561c40bdb31f49c6f1786662c30a6f9cc0
Sivantha Paranavithana <sivantha.p@eatmeglobal.com>
2024-11-02 07:14:51 +0530

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Sivantha Paranavithana on 2024-11-02.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
